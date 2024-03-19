82570581a1d26277dac450379361af978f1dd5d5
Yuganshu Kumar <69767140+YTheLynch@users.noreply.github.com>
2024-03-19 23:50:57 +0530

    Working Whisper set up complete

---
+++ README.md
@@ -0,0 +1,3 @@
+## Installation:
+1. Run `choco install ffmpeg` (install latest version of ffmpeg and add it to PATH)
+2. In shell, run `pip install -r requirements.txt`
--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 # Trying whisper out 
 
-from openai import OpenAI
-client = OpenAI()
+
+import whisper
+model = whisper.load_model("base")
+result = model.transcribe("./Recording (4).m4a", fp16 = False)
+print(f' The text in video: \n {result["text"]}')
+
