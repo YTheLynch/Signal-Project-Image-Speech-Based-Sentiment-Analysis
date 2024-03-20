4b68477d1c4f3286e70eadc19a53943e6f3dd87b
Yuganshu Kumar <69767140+YTheLynch@users.noreply.github.com>
2024-03-20 20:30:18 +0530

    DistilBERT Tests

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 # Trying whisper out 
 
 
-import whisper
-model = whisper.load_model("base")
-result = model.transcribe("./Recording (4).m4a", fp16 = False)
-print(f' The text in video: \n {result["text"]}')
+# import torch, whisper
+# device = 'cuda' if torch.cuda.is_available() else 'cpu'
+# model = whisper.load_model("medium.en").to(device)
+# result = model.transcribe("./Test Audios/Recording (4).m4a")
+# print(f' The text in video: \n {result["text"]}')
 
+
+# Sentiment Analysis
+from pprint import pprint
+
+from transformers import pipeline
+sentiment_pipeline = pipeline("text-classification", model='bhadresh-savani/distilbert-base-uncased-emotion', top_k = None)
+data = ["I love you", "I hate you"]
+res = sentiment_pipeline(data)
+pprint(res)
